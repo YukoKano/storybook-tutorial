0b80f954ac614968158a4d643dfa286b7860ca95
YukoKano <halloween_1031@hotmail.co.jp>
2024-07-10 08:20:03 +0900

    Taskコンポーネントを変更

--- taskbox/src/components/Task.jsx
+++ taskbox/src/components/Task.jsx
@@ -4,7 +4,22 @@ export default function Task({
   onPinTask,
 }) {
   return (
-    <div className="list-item">
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor={`archiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+
       <label htmlFor={`title-${id}`} aria-label={title}>
         <input
           type="text"
@@ -12,8 +27,20 @@ export default function Task({
           readOnly={true}
           name="title"
           id={`title-${id}`}
+          placeholder="Input title"
         />
       </label>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
     </div>
   );
 }
